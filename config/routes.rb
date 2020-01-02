22d71d778389e703d6e7c049bc75734890342f31
Shirasaka-Takahiro <kenshirou913@gmail.com>
2020-01-02 19:31:46 +0000

    finished works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
+  get 'about/show'
   root to: 'home#index'
 
   resources :homes
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_064434) do
+ActiveRecord::Schema.define(version: 2020_01_02_162957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abouts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
